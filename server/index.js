0f7fa333dd31e54cc753e338597ffc555c7b475a
Aritra Mondal <sasuke.aritra.sa@gmail.com>
2023-02-25 11:51:33 +0530

    openai added

--- server/index.js
+++ server/index.js
@@ -1,14 +1,23 @@
 const express = require("express");
+const { Configuration, OpenAIApi } = require("openai");
 const dotenv = require("dotenv");
 const cors = require("cors");
+const askRoutes=require('./routes/askRoutes')
 dotenv.config();
 
 
 const app = express();
 app.use(cors()); //actual link will be added later
 app.use(express.json());
+app.use(askRoutes);
 
 const port = process.env.PORT || 5000;
+const OPENAI_API_KEY = process.env.OPENAI_KEY;
+
+const configuration = new Configuration({
+    apiKey: OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
 
 app.listen(port, () => {
     console.log(`SERVER IS LIVE AND RUNNING AT :-> ${port}`);
--- server/routes/askRoutes.js
+++ server/routes/askRoutes.js
@@ -1,3 +1,37 @@
 const express = require("express");
 const router = express.Router();
 
+router.post("/chat", (req, res) => {
+    const question = req.body.question;
+  
+    openai
+      .createCompletion({
+        model: "text-davinci-003",
+        prompt: question,
+        max_tokens: 1000,
+        temperature: 0,
+      })
+      .then((response) => {
+        console.log(response?.data?.choices?.[0]?.text);
+        return response?.data?.choices?.[0]?.text;
+      })
+      .then((answer) => {
+        const array = answer
+          ?.split("\n")
+          .filter((value) => value)
+          .map((value) => value.trim());
+  
+        return array;
+      })
+      .then((answer) => {
+        res.json({
+          answer: answer,
+          prompt: question,
+        });
+      });
+  
+    //   console.log({ question });
+  });
+  
+
+module.exports=router
