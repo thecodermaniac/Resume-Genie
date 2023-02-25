79d4f4e90cec704e3ebf1f3b49a85fd4d05003f5
Gopesh Goswami <ggopesh45@gmail.com>
2023-02-26 03:21:17 +0530

    just commit

--- server/openAI/openapi.js
+++ server/openAI/openapi.js
@@ -2,8 +2,8 @@ const { Configuration, OpenAIApi } = require("openai");
 const OPENAI_API_KEY = process.env.OPENAI_KEY;
 
 const configuration = new Configuration({
-    apiKey: "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",
+  apiKey: "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",
 });
 const openai = new OpenAIApi(configuration);
 
-module.exports = openai
+module.exports = openai;
