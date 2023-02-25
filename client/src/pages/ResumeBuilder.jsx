e4885c07757c806061f0c248992d906b3ba5b2bf
Gopesh Goswami <ggopesh45@gmail.com>
2023-02-26 04:15:25 +0530

    ResumeBuilder Fix pending

--- client/src/pages/ResumeBuilder.jsx
+++ client/src/pages/ResumeBuilder.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
+import bgBlur from "../images/blur.png";
 
 import Loading from "../components/Loader";
 
@@ -50,116 +51,131 @@ const ResumeBuilder = ({ setResult }) => {
     setLoading(true);
   };
   return (
-    <div className="border-2 border-red-500">
-      <h1 className=" text-2xl font-bold mb-4">Resume Builder</h1>
-      <p className=" text-lg mb-4">
-        Generate a resume with ChatGPT in few seconds
-      </p>
-      <form
-        onSubmit={handleFormSubmit}
-        method="POST"
-        encType="multipart/form-data"
-      >
-        <label htmlFor="fullName" className=" text-lg font-semibold">
-          Enter your full name
-        </label>
-        <input
-          type="text"
-          required
-          name="fullName"
-          id="fullName"
-          value={fullName}
-          onChange={(e) => setFullName(e.target.value)}
-        />
-        <div className="nestedContainer">
-          <div>
-            <label htmlFor="currentPosition">Current Position</label>
+    <div className=" flex flex-row mx-10 border-2 border-red-500 h-[calc(100vh-8rem)] rounded-xl justify-center ">
+      <aside className=" hidden md:flex w-1/3 bg-gradient-to-tr from-teal-500 to-teal-600 rounded-l-xl">
+        <div className=" mt-10 md:mx-10 flex flex-col justify-around h-full">
+          <h1 className=" text-5xl font-bold mb-10">Resume Builder</h1>
+          {/* Card Start */}
+          <div className=" bg-teal-600 shadow-md px-10 py-10 rounded-lg">
+            <p className=" text-xl mb-4">
+              Generate a resume with ChatGPT in few seconds
+            </p>
+          </div>
+          {/* Card End */}
+        </div>
+      </aside>
+      <aside className=" mx-0 md:mx-10 flex justify-center">
+        <form
+          className=" border-2 border-red-500"
+          onSubmit={handleFormSubmit}
+          method="POST"
+          encType="multipart/form-data"
+        >
+          <div className=" flex flex-col mb-5">
+            <label htmlFor="fullName" className=" text-lg font-semibold">
+              Enter your full name
+            </label>
             <input
               type="text"
               required
-              name="currentPosition"
-              className="currentInput"
-              value={currentPosition}
-              onChange={(e) => setCurrentPosition(e.target.value)}
+              name="fullName"
+              id="fullName"
+              value={fullName}
+              onChange={(e) => setFullName(e.target.value)}
+              className=" px-5 py-3 text-black"
             />
           </div>
-          <div>
-            <label htmlFor="currentLength">For how long? (year)</label>
-            <input
-              type="number"
-              required
-              name="currentLength"
-              className="currentInput"
-              value={currentLength}
-              onChange={(e) => setCurrentLength(e.target.value)}
-            />
+
+          <div className="nestedContainer border-2 border-red-500 mb-5">
+            <div>
+              <label htmlFor="currentPosition">Current Position</label>
+              <input
+                type="text"
+                required
+                name="currentPosition"
+                className="w-full border-2 border-red-500"
+                value={currentPosition}
+                onChange={(e) => setCurrentPosition(e.target.value)}
+              />
+            </div>
+            <div className=" ml-10">
+              <label htmlFor="currentLength">For how long? (year)</label>
+              <input
+                type="number"
+                required
+                name="currentLength"
+                className="w-full border-2 border-red-500"
+                value={currentLength}
+                onChange={(e) => setCurrentLength(e.target.value)}
+              />
+            </div>
           </div>
-          <div>
+          <div className="  flex flex-col mb-5">
             <label htmlFor="currentTechnologies">Technologies used</label>
             <input
               type="text"
               required
               name="currentTechnologies"
-              className="currentInput"
+              className=" w-full "
               value={currentTechnologies}
               onChange={(e) => setCurrentTechnologies(e.target.value)}
             />
           </div>
-        </div>
-        <label htmlFor="photo">Upload your headshot image</label>
-        <input
-          type="file"
-          name="photo"
-          required
-          id="photo"
-          accept="image/x-png,image/jpeg"
-          onChange={(e) => setHeadshot(e.target.files[0])}
-        />
 
-        <h3>Companies you've worked at</h3>
+          {companyInfo.map((company, index) => (
+            <div className="nestedContainer" key={index}>
+              <div className="companies">
+                <label htmlFor="name">Company Name</label>
+                <input
+                  type="text"
+                  name="name"
+                  required
+                  onChange={(e) => handleUpdateCompany(e, index)}
+                />
+              </div>
+              <div className="companies">
+                <label htmlFor="position">Position Held</label>
+                <input
+                  type="text"
+                  name="position"
+                  required
+                  onChange={(e) => handleUpdateCompany(e, index)}
+                />
+              </div>
 
-        {companyInfo.map((company, index) => (
-          <div className="nestedContainer" key={index}>
-            <div className="companies">
-              <label htmlFor="name">Company Name</label>
-              <input
-                type="text"
-                name="name"
-                required
-                onChange={(e) => handleUpdateCompany(e, index)}
-              />
+              <div className="btn__group">
+                {companyInfo.length - 1 === index && companyInfo.length < 4 && (
+                  <button id="addBtn" onClick={handleAddCompany}>
+                    Add
+                  </button>
+                )}
+                {companyInfo.length > 1 && (
+                  <button
+                    id="deleteBtn"
+                    onClick={() => handleRemoveCompany(index)}
+                  >
+                    Del
+                  </button>
+                )}
+              </div>
             </div>
-            <div className="companies">
-              <label htmlFor="position">Position Held</label>
-              <input
-                type="text"
-                name="position"
-                required
-                onChange={(e) => handleUpdateCompany(e, index)}
-              />
-            </div>
-
-            <div className="btn__group">
-              {companyInfo.length - 1 === index && companyInfo.length < 4 && (
-                <button id="addBtn" onClick={handleAddCompany}>
-                  Add
-                </button>
-              )}
-              {companyInfo.length > 1 && (
-                <button
-                  id="deleteBtn"
-                  onClick={() => handleRemoveCompany(index)}
-                >
-                  Del
-                </button>
-              )}
-            </div>
+          ))}
+          <div className=" flex flex-col mt-5 w-full">
+            <label htmlFor="photo">Upload your headshot image</label>
+            <input
+              type="file"
+              name="photo"
+              required
+              id="photo"
+              accept="image/x-png,image/jpeg"
+              onChange={(e) => setHeadshot(e.target.files[0])}
+              className=" w-full border-none p-0 mt-3 "
+            />
           </div>
-        ))}
-
-        {!loading && <button>CREATE RESUME</button>}
-        {loading && <Loading />}
-      </form>
+          {!loading && <button>CREATE RESUME</button>}
+          {loading && <Loading />}
+        </form>
+      </aside>
     </div>
   );
 };
--- server/routes/analysisRoutes.js
+++ server/routes/analysisRoutes.js
@@ -1,76 +1,72 @@
 const express = require("express");
 const router = express.Router();
 const path = require("path");
-const openai = require('../openAI/openapi')
+const openai = require("../openAI/openapi");
 const multer = require("multer");
-const fs = require('fs')
-const pdf = require('pdf-parse')
-
+const fs = require("fs");
+const pdf = require("pdf-parse");
 
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, "pdfUploads");
-    },
-    filename: (req, file, cb) => {
-        cb(null, Date.now() + path.extname(file.originalname));
-    },
+  destination: (req, file, cb) => {
+    cb(null, "pdfUploads");
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + path.extname(file.originalname));
+  },
 });
 const upload = multer({
-    storage: storage
+  storage: storage,
 });
 
-
 router.post("/resume/analyse", upload.single("pdffile"), async (req, res) => {
-    try {
-        console.log(req.file.filename);
-        console.log(path.join('pdfUploads', req.file.filename));
-        var pathvalue = path.join('pdfUploads', req.file.filename)
-        let dataBuffer = fs.readFileSync(pathvalue);
-        pdf(dataBuffer).then(function (data) {
-            // console.log(data.text)
+  try {
+    console.log(req.file.filename);
+    console.log(path.join("pdfUploads", req.file.filename));
+    var pathvalue = path.join("pdfUploads", req.file.filename);
+    let dataBuffer = fs.readFileSync(pathvalue);
+    pdf(dataBuffer).then(function (data) {
+      // console.log(data.text)
 
-            // openai code goes here
+      // openai code goes here
 
-            openai
-                .createCompletion({
-                    model: "text-davinci-003",
-                    prompt: "Give me review of this resume: " + data.text,
-                    max_tokens: 1000,
-                    temperature: 0,
-                })
-                .then((response) => {
-                    console.log(response?.data?.choices?.[0]?.text);
-                    return response?.data?.choices?.[0]?.text;
-                })
-                .then((answer) => {
-                    const array = answer
-                        ?.split("\n")
-                        .filter((value) => value)
-                        .map((value) => value.trim());
-
-                    return array;
-                })
-                .then((answer) => {
-                    console.log(answer);
-                    res.json({
-                        answer: answer
-                    });
-                }).catch((error) => {
-                    res.status(error.statusCode || 500).json({
-                        success: false,
-                        message: error.message,
-                    });
-                })
-
-
+      openai
+        .createCompletion({
+          model: "text-davinci-003",
+          prompt: "Give me review of this resume: " + data.text,
+          max_tokens: 1000,
+          temperature: 0,
         })
-    } catch (error) {
-        res.status(error.statusCode || 500).json({
+        .then((response) => {
+          console.log(response?.data?.choices?.[0]?.text);
+          return response?.data?.choices?.[0]?.text;
+        })
+        .then((answer) => {
+          const array = answer
+            ?.split("\n")
+            .filter((value) => value)
+            .map((value) => value.trim());
+
+          return array;
+        })
+        .then((answer) => {
+          console.log(answer);
+          res.json({
+            answer: answer,
+          });
+        })
+        .catch((error) => {
+          res.status(error.statusCode || 500).json({
             success: false,
             message: error.message,
+          });
         });
-    }
-
-})
+    });
+  } catch (error) {
+    res.status(error.statusCode || 500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+});
 
 module.exports = router;
