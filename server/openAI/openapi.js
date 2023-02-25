6f5cec1628c2378f076565e9177f2659fd241725
Aritra Mondal <sasuke.aritra.sa@gmail.com>
2023-02-26 03:36:20 +0530

    openai issue solved

--- server/openAI/openapi.js
+++ server/openAI/openapi.js
@@ -1,9 +1,12 @@
 const { Configuration, OpenAIApi } = require("openai");
+const dotenv = require("dotenv");
+dotenv.config();
+
 const OPENAI_API_KEY = process.env.OPENAI_KEY;
 
 const configuration = new Configuration({
   apiKey:
-    OPENAI_API_KEY || "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",
+    OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
